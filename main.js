d1912b3ebc1d5080187fb4ac4bb40ae2b66977b0
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-03-17 13:58:04 -0600

    1 & 2

---
+++ main.js
@@ -0,0 +1,19 @@
+const lineThru = document.querySelectorAll("#ol");
+
+for (const value of lineThru){
+    value.addEventListener('click', function (event){
+        console.log(event);
+        const elementThatWasClicked = event.target;
+        elementThatWasClicked.style.textDecoration = "line-through";
+    });
+} 
+
+const opac = document.querySelectorAll("#ul");
+
+for (const ul of opac){
+    ul.addEventListener("click", function (event){
+        console.log(event);
+        const elementThatWasClicked = event.target;
+        elementThatWasClicked.style.opacity = "0";
+    });
+} 
